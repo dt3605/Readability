fbc6fc6e7d420a3bc16e0f10a17bbe1d819bfadb
Davi <daviteod2019@icloud.com>
2025-08-11 16:53:17 -0300

    Add files via upload

---
+++ readability.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+int main(void)
+{
+    char text[1000];
+
+    printf("Text: ");
+    fgets(text, sizeof(text), stdin);
+
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+            letters++;
+
+        if (text[i] == ' ')
+            words++;
+
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+            sentences++;
+    }
+
+    double L = (double) letters / words * 100;
+    double S = (double) sentences / words * 100;
+
+    double index = 0.0588 * L - 0.296 * S - 15.8;
+    int grade = (int) round(index);
+
+    if (grade < 1)
+        printf("Before Grade 1\n");
+    else if (grade >= 16)
+        printf("Grade 16+\n");
+    else
+        printf("Grade %d\n", grade);
+
+    return 0;
+}
